a8105bd532eed20c600c25244327453474c66cb7
shawnmcclure <shawn850mcclure@gmail.com>
2023-08-23 22:25:22 -0500

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    const newCats = [...cats,name]  
+    return newCats
+}
+function prependCat(name){
+    const newCats = [name,...cats]
+    return newCats
+}
+function removeLastCat(){
+    return cats.slice(0,2)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
